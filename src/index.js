6ad63f2c378e9784a48336e6024fcbad8846ede8
Tiago Musumeci <tiagomusu@gmail.com>
2022-06-12 11:52:11 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { GifExpertApp } from './GifExpertApp';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<GifExpertApp />);
